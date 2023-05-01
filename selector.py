c95cbdeef3b39589d14cc373d3dd34b12b706a44
KAPOKA00 <kwakjm613@naver.com>
2023-05-01 14:42:45 +0900

    Feat: Done get_lucky_num()

--- selector.py
+++ selector.py
@@ -1,5 +1,11 @@
 import random
 
-result = random.sample(range(1, 46), k=6)
-print(result)
 
+def get_lucky_nums():
+  result = random.sample(range(1, 46), k=6)
+  return result
+
+if __name__ == '__main__':
+  times = int(input("Enter num(1-100): "))
+  for _ in range(times):
+    print(get_lucky_nums())
